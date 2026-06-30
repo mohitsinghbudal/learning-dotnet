944262cc1def1c84e348fd628da8a4305dbea718
Mohit Singh Budal <budalmohitsingh@gmail.com>
2026-06-30 14:23:54 +0545

    merge conflict test

--- Li-copy/WeatherForecast.cs
+++ Li-copy/WeatherForecast.cs
@@ -8,6 +8,6 @@ namespace Li_copy
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string? Summarys { get; set; }
     }
 }
